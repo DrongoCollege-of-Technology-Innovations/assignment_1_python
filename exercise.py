56c1af0e834c208e71148565465c40dd909f95a0
kelvinokumu <oumakelvinokumu.com>
2023-02-21 12:47:17 +0300

    assignment

---
+++ exercise.py
@@ -0,0 +1,1 @@
+# write a function names get_sum() that add two numbers and returns a value of 75
---
+++ test_exercise.py
@@ -0,0 +1,9 @@
+from exercise import get_sum
+
+def test_get_sum():
+    assert get_sum() == 75, "Error"
+
+
+if __name__ == '__main__':
+    test_get_sum()
+    print("Test passed")
